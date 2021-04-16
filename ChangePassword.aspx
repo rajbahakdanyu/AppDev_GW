f292ccb1964a5b29150009e008669f953c055b0a
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-04-16 18:20:52 +0545

    Added password reset function

--- ChangePassword.aspx
+++ ChangePassword.aspx
@@ -1,4 +1,15 @@
 ﻿<%@ Page Title="Change Password" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ChangePassword.aspx.cs" Inherits="AppDev_GW.ChangePassword" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="lbPassword" runat="server" Text="Enter New Password"></asp:Label>
+    <br />
+    <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="lbConfirm" runat="server" Text="Confirm Password"></asp:Label>
+    <br />
+    <asp:TextBox ID="txtConfirm" runat="server"></asp:TextBox>
+    <br /><br />
+    <asp:Button ID="btnChange" runat="server" Text="Change Password" OnClick="btnChange_Click" />
+    &nbsp;
+    <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
 </asp:Content>
--- ChangePassword.aspx.cs
+++ ChangePassword.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,62 @@ namespace AppDev_GW
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnChange_Click(object sender, EventArgs e)
+        {
+            string password = txtPassword.Text;
+            string confirm = txtConfirm.Text;
+
+            if (confirm != "" && password != "")
+            {
+                if (confirm == password)
+                {
+                    Change(confirm);
+                }
+                else
+                {
+                    Response.Write("<script language=javascript>alert('Both password do not match')</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script language=javascript>alert('Please password in both fields')</script>");
+            }
+        }
+
+        protected void Change(string password)
+        {
+            string id = Session["id"].ToString();
+
+            String connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            String queryString = $"UPDATE [User] SET [Password] = '{password}' WHERE [Id] = {id}";
+
+            // Connecting to database
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(queryString, con);
+                con.Open();
+
+                // Executing query
+                int i = cmd.ExecuteNonQuery();
+
+                if (i == 1)
+                {
+                    Response.Redirect("Default.aspx");
+                    Response.Write("<script language=javascript>alert('Password Changed Successfully')</script>");
+                } else
+                {
+                    Response.Write("<script language=javascript>alert('Could not change password')</script>");
+                }
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtConfirm.Text = "";
+            txtPassword.Text = "";
         }
     }
 }
