06959bb0fb7d97b678402213036ef679c17a7661
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-04-24 19:42:38 +0545

    Added code for inactive customers

---
+++ Inactive.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppDev_GW
+{
+    public partial class Inactive : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            String queryString = @"SELECT *
+                                    FROM Customer
+                                    WHERE NOT EXISTS ( 
+                                            SELECT *
+                                            FROM [Order]
+                                            WHERE [CustomerId] = [Customer].[Id]
+                                            AND [Date] >= Dateadd(DAY, -31, sysdatetime())
+                                    )";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queryString, con);
+
+                DataTable dt = new DataTable("Customers");
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    dt.Load(sdr);
+                }
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
