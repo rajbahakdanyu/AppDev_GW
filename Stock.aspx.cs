7fbd88964feb94450a66d00a8ce79567ed8e6ad0
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-04-25 22:12:45 +0545

    Added update stock function

--- Stock.aspx.cs
+++ Stock.aspx.cs
@@ -20,7 +20,7 @@ namespace AppDev_GW
             }
         }
 
-        private void BindGrid()
+        protected void BindGrid()
         {
             string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
@@ -42,6 +42,89 @@ namespace AppDev_GW
 
                 GridView1.DataSource = dt;
                 GridView1.DataBind();
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            int item = Convert.ToInt32(ddlItem.SelectedValue);
+
+            if (txtQuantity.Text != "")
+            {
+                try
+                {
+                    int quantity = Convert.ToInt32(txtQuantity.Text);
+
+                    if (quantity > 0)
+                    {
+                        int remaining = getQuantity(item);
+                        int total = remaining + quantity;
+
+                        restock(item, total);
+                    }
+                    else
+                    {
+                        Response.Write($"<script language=javascript>alert('Quantity must be more than 0')</script>");
+                    }
+                } catch(FormatException ex)
+                {
+                    Response.Write($"<script language=javascript>alert('Quantity must be a number')</script>");
+                }  
+            } else
+            {
+                Response.Write($"<script language=javascript>alert('Please Enter Quantity')</script>");
+            }
+        }
+
+        protected int getQuantity(int Id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            String queryString = $"SELECT [Quantity] FROM [Stock] WHERE ItemId = {Id}";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queryString, con);
+
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        return Convert.ToInt32(sdr["Quantity"]);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        protected void restock(int Id, int quantity)
+        {
+            try
+            {
+                var current = DateTime.Today;
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                String queryString = $"UPDATE [Stock] SET [Quantity] = {quantity}, [Date] = '{current}'  WHERE ItemId = {Id}";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(queryString, con);
+
+                    int sdr = cmd.ExecuteNonQuery();
+
+                    if (sdr == 1)
+                    {
+                        Response.Write($"<script language=javascript>alert('Item was successfully restocked')</script>");
+                        txtQuantity.Text = "";
+                        BindGrid();
+                    }
+                }
+
+            } catch (Exception ex)
+            {
+                Response.Write($"<script language=javascript>alert('Error with Database')</script>");
             }
         }
     }
