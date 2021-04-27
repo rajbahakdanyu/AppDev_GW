442a82102b57374b4cee29ba6f929b2ffa0c9124
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-04-27 17:51:58 +0545

    Added sales function

--- Sale.aspx
+++ Sale.aspx
@@ -1,5 +1,73 @@
 ﻿<%@ Page Title="Sale" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Sale.aspx.cs" Inherits="AppDev_GW.Sale" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+        <asp:Label ID="lbTitle" runat="server" Text="Sales" Font-Size="Large"></asp:Label>
 
+    <div>
+        <table>
+            <tr>
+                <td><asp:Label ID="lbName" runat="server" Text="Product Name"></asp:Label></td>
+                <td><asp:DropDownList ID="ddlItem" runat="server" DataSourceID="SqlDataSource1" DataTextField="Name" DataValueField="Id"></asp:DropDownList>
+                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [Id], [Name] FROM [Item]"></asp:SqlDataSource></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="lbCustomer" runat="server" Text="Customer Name"></asp:Label></td>
+                <td><asp:DropDownList ID="ddlCustomer" runat="server" DataSourceID="SqlDataSource2" DataTextField="Name" DataValueField="Id"></asp:DropDownList>
+                <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [Id], [Name] FROM [Customer]"></asp:SqlDataSource></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="lbQuantity" runat="server" Text="Quantity"></asp:Label></td>
+                <td><asp:TextBox ID="txtQuantity" runat="server"></asp:TextBox></td>
+            </tr>
+             <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                </td>
+                </tr>
+        </table>
+    </div>
+
+    <div>
+        <asp:GridView ID="GridView1" runat="server" EmptyDataText="Nothing has been sold yet"></asp:GridView>
+    </div>
+
+         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h3 class="modal-title" id="exampleModalLabel">Invoice</h3>
+          </div>
+          <div class="modal-body">
+            <div id="dialog">
+                <table>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Customer Name</th>
+                        <th>Product Quantity</th>
+                        <th>Total Amount</th>
+                    </tr>
+                    <tr>
+                        <td><asp:Label ID="Label1" runat="server" Text=""></asp:Label></td>
+                        <td><asp:Label ID="Label2" runat="server" Text=""></asp:Label></td>
+                        <td><asp:Label ID="Label3" runat="server" Text=""></asp:Label></td>
+                        <td><asp:Label ID="Label4" runat="server" Text=""></asp:Label></td>
+                    </tr>
+                </table>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <asp:Button ID="btnConfirm" class="btn btn-primary" runat="server" Text="Yes, Proceed" OnClick="btnConfirm_Click" />
+            <asp:Button ID="btnCancel" class="btn btn-secondary" runat="server" Text="cancel" data-dismiss="modal" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script type="text/javascript">
+        function showModal() {
+            $("#exampleModal").modal('show');
+        }
+    </script>
 </asp:Content>
--- Sale.aspx.cs
+++ Sale.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,187 @@ namespace AppDev_GW
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                BindGrid();
+            }
+        }
 
+        protected void BindGrid()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            String queryString = @"SELECT * FROM [Order]";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queryString, con);
+
+                DataTable dt = new DataTable("Sales");
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    dt.Load(sdr);
+                }
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+
+        int total, customer, item, quantity;
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            customer = Convert.ToInt32(ddlCustomer.SelectedValue);
+            item = Convert.ToInt32(ddlItem.SelectedValue);
+
+            if (txtQuantity.Text != "")
+            {
+                try
+                {
+                    quantity = Convert.ToInt32(txtQuantity.Text);
+
+                    if (quantity > 0)
+                    {
+                        int remaining = getQuantity(item);
+
+                        if (remaining == 0)
+                        {
+                            Response.Write($"<script language=javascript>alert('Sorry the product is out of stock')</script>");
+                        } else if (quantity > remaining)
+                        {
+                            Response.Write($"<script language=javascript>alert('Requested amount is greater than stock')</script>");
+                        } else
+                        {
+                            total = getPrice(item) * quantity;
+
+                            display();
+                        }
+                    }
+                    else
+                    {
+                        Response.Write($"<script language=javascript>alert('Quantity must be more than 0')</script>");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Response.Write($"<script language=javascript>alert('Quantity must be a number')</script>");
+                }
+            }
+            else
+            {
+                Response.Write($"<script language=javascript>alert('Please Enter Quantity')</script>");
+            }
+        }
+
+        protected int getQuantity(int Id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            String queryString = $"SELECT [Quantity] FROM [Stock] WHERE ItemId = {Id}";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queryString, con);
+
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        return Convert.ToInt32(sdr["Quantity"]);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        protected int getPrice(int Id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            String queryString = $"SELECT [Price] FROM [item] WHERE [Id] = {Id}";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queryString, con);
+
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        return Convert.ToInt32(sdr["Price"]);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        protected void display()
+        {
+            Label1.Text = ddlItem.SelectedItem.Text;
+            Label2.Text = ddlCustomer.SelectedItem.Text;
+            Label3.Text = quantity.ToString();
+            Label4.Text = total.ToString();
+            ClientScript.RegisterStartupScript(this.GetType(), "Pop", "showModal();", true);
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+                var current = DateTime.Today;
+                customer = Convert.ToInt32(ddlCustomer.SelectedValue);
+                item = Convert.ToInt32(ddlItem.SelectedValue); 
+                quantity = Convert.ToInt32(txtQuantity.Text); 
+                total = getPrice(item) * quantity;
+
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                String queryString = $"INSERT INTO [Order] ([Order].[CustomerId], [Order].[ItemId], [Quantity], [TotalAmt], [Date]) VALUES ({customer}, {item}, {quantity}, {total}, '{current}')";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(queryString, con);
+
+                    cmd.ExecuteNonQuery();
+
+                    Response.Write($"<script language=javascript>alert('Item was sold successfully')</script>");
+                    txtQuantity.Text = "";
+                    BindGrid();
+                    updateStock(item, quantity);
+                }
+
+        }
+
+        protected void updateStock(int Id, int quantity)
+        {
+            try
+            {
+                int remaining = getQuantity(Id) - quantity;
+
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                String queryString = $"UPDATE [Stock] SET [Quantity] = {remaining}  WHERE ItemId = {Id}";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(queryString, con);
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script language=javascript>alert('Error with Database')</script>");
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtQuantity.Text = "";
         }
     }
 }
