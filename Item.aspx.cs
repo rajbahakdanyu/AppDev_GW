9526eee6864fa7f12622de6feb3685b5d0815949
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-04-27 13:44:36 +0545

    Updated the insert query iin items

--- Item.aspx.cs
+++ Item.aspx.cs
@@ -20,44 +20,40 @@ namespace AppDev_GW
         {
             String connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
-
             string itemID = txtItemID.Text.ToString() ?? "";
             string itemName = txtItemName.Text.ToString() ?? "";
             string itemDescription = txtItemDescription.Text.ToString() ?? "";
             int itemPrice = txtItemPrice.Text.ToString().Length != 0 ? Int32.Parse(txtItemPrice.Text.ToString()) : 0;
             string itemPurchaseDate = txtItemPurchaseDate.Text.ToString() ?? "";
             string itemCatagory = ddlCatagory.Text.ToString() ?? "";
-            string itemMenufactureDate = txtItemManufactureDate.Text.ToString() ?? "";
+            string itemManufactureDate = txtItemManufactureDate.Text.ToString() ?? "";
             string itemExpiryDate = txtItemExpiryDate.Text.ToString() ?? "";
             String queryString = "";
+
             if (btnAddItem.Text == "Insert")
             {
-                queryString = $"INSERT INTO ITEM (NAME,DESCRIPTION,PRICE,PURCHASE,CATAGORY,MANUFACTURE,EXPIRY) VALUES('{itemName}','{itemDescription}',{itemPrice}, TO_DATE('{itemPurchaseDate}','dd/mm/yyyy')',{itemCatagory}, TO_DATE('{itemMenufactureDate}','dd/mm/yyyy')', TO_DATE('{itemExpiryDate}','dd/mm/yyyy')')";
-
-
+                queryString = $"INSERT INTO [Item] ([Name], [Description], [Price], [Purchase], [Category], [Manufacture], [Expiry]) VALUES ('{itemName}', '{itemDescription}', {itemPrice}, '{itemPurchaseDate}', {itemCatagory}, '{itemManufactureDate}', '{itemExpiryDate}')";
             }
             else if (btnAddItem.Text == "Update")
             {
                 //Make the text field hidden.
                 String ID = txtItemID.Text.ToString();
-                queryString = $"INSERT INTO ITEM (NAME,DESCRIPTION,PRICE,PURCHASE,CATAGORY,MANUFACTURE,EXPIRY) VALUES('{itemName}','{itemDescription}',{itemPrice}, TO_DATE('{itemPurchaseDate}','dd/mm/yyyy')',{itemCatagory}, TO_DATE('{itemMenufactureDate}','dd/mm/yyyy')', TO_DATE('{itemExpiryDate}','dd/mm/yyyy')')";
-
+                queryString = $"UPDATE [Item] SET [Name] = '{itemName}', [Description] = '{itemDescription}', [Price] = {itemPrice}, [Purchase] = 'TO_DATE('{itemPurchaseDate}','dd/mm/yyyy')', [Category] = {itemCatagory}, [Manufacture] = 'TO_DATE('{itemManufactureDate}','dd/mm/yyyy')', [Expiry] = 'TO_DATE('{itemExpiryDate}','dd/mm/yyyy')' WHERE Id = {itemID}";
             }
 
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                //SqlCommand cmd = new SqlCommand(queryString, con);
-                //con.Open();
-                //cmd.ExecuteReader();
-                Response.Write($"<script language=javascript>alert('{btnAddItem.Text}')</script>");
+                SqlCommand cmd = new SqlCommand(queryString, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Response.Write($"<script language=javascript>alert('Performed operation successfully.')</script>");
             }
             btnAddItem.Text = "Insert";
 
-            //itemsGridView.EditIndex = -1;
             itemsGridView.EditIndex = -1;
             this.load_data();
-
         }
+
         public void load_data()
         {
             try
@@ -81,24 +77,24 @@ namespace AppDev_GW
             }
         }
 
-
         protected void OnRowCancelingEdit(object sender, EventArgs e)
         {
-            //itemsGridView.EditIndex = -1;
             itemsGridView.EditIndex = -1;
             btnAddItem.Text = "Insert";
             this.load_data();
             btnCancleUpdate.Visible = true;
         }
+
         protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
         {
             string ID = itemsGridView.DataKeys[e.RowIndex].Values[0].ToString();
             string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            string queryString = $"DELETE FROM [Item] WHERE [Id] = {ID}";
+
             using (SqlConnection con = new SqlConnection(constr))
             {
-                using (SqlCommand cmd = new SqlCommand("DELETE FROM ITEM WHERE ID ='" + ID + "' "))
+                using (SqlCommand cmd = new SqlCommand(queryString))
                 {
-
                     cmd.Connection = con;
                     con.Open();
                     cmd.ExecuteNonQuery();
@@ -107,6 +103,7 @@ namespace AppDev_GW
             }
             this.load_data();
         }
+
         protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
         {
             if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != itemsGridView.EditIndex)
@@ -114,8 +111,8 @@ namespace AppDev_GW
                 (e.Row.Cells[0].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
             }
             btnCancleUpdate.Visible = false;
-
         }
+
         protected void OnRowEditing(object sender, GridViewEditEventArgs e)
         {
             //itemsGridView.EditIndex = e.NewEditIndex;
